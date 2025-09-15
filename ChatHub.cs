856fb83821cd59aa2c8fc7bb7d0f1397153681bc
Roger Ccopa <rogerccopa@gmail.com>
2025-09-15 10:50:15 -0500

    Basic signalR chat working

--- ChatHub.cs
+++ ChatHub.cs
@@ -3,7 +3,13 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRDemo;
 
+// A Hub is the central piece where clients and server send messages.
 public class ChatHub : Hub
 {
-    // CONTINUE HERE...
+    // Method clients can call
+    public async Task SendMessage(string user, string message)
+    {
+        // Broadcasts to all connected clients
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using SignalRDemo;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add SignalR
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();  // Serve index.html by default
+app.UseStaticFiles();   // Serve static files in wwwroot (JS, HTML, CSS)
+
+app.MapHub<ChatHub>("/TheChatHub");    // Map the SignalR hub to this path
 
 app.Run();
